d6852ef9e34001b22a34a77e0f2af9e2f134e85d
zzzzzzian-20 <133369151+zzzzzzian-20@users.noreply.github.com>
2023-05-14 16:13:00 +0800

    Update view_daily_record.py

--- wxcloudrun/view_daily_record.py
+++ wxcloudrun/view_daily_record.py
@@ -1,10 +1,16 @@
 from flask import request
 from marshmallow import Schema, fields
 from marshmallow.validate import Length
+from marshmallow.validate import Range 
+from marshmallow.validate import OneOf 
 from sqlalchemy.engine import Engine
 from sqlalchemy import select
 from sqlalchemy import desc
+from sqlalchemy import asc
+from sqlalchemy import func
 from webargs.flaskparser import use_kwargs
+from sqlalchemy import and_
+from sqlalchemy import nulls_last
 
 from run import app
 from wxcloudrun import db
@@ -37,3 +43,83 @@ def new_wish(wish: str):
         .limit(1)
     ).fetchall()
   return make_succ_response({'id': res[0][0]})
+
+
+class WishList(Schema):
+  mode = fields.String(load_default='list',
+                       validate=[OneOf(('list', 'last'))])
+  last_id = fields.Integer(load_default=0, 
+                           validate=[Range(min=0)])
+  page_num = fields.Integer(load_default=5, 
+                            validate=[Range(min=1, max=20)])
+  fulfill = fields.Boolean(load_default=False)
+
+
+WISH_FIELDS = ('id', 'create_time', 'update_time', 'last_time', 
+               'count', 'fulfill', 'wish')
+
+
+@app.route('/api/wooden_fish/wish/list', 
+           methods=['POST'])
+@use_kwargs(WishList)
+def wish_list(mode: str, last_id: int, page_num: int, fulfill: bool):
+  engine: Engine = db.engine
+  table = wish_table.table
+  openid = request.headers.get('X-WX-OPENID')
+
+  sql = select(table.c.id,
+               func.unix_timestamp(table.c.create_time).label('create_time'),
+               func.unix_timestamp(table.c.update_time).label('update_time'),
+               func.unix_timestamp(table.c.last_time).label('last_time'),
+               table.c.count,
+               table.c.fulfill,
+               table.c.wish)
+  if mode == 'list':
+    sql = (
+        sql
+        .where(and_(table.c.fulfill.is_(fulfill),
+                    table.c.openid == openid,
+                    table.c.id > last_id))
+        .order_by(asc(table.c.id))
+        .limit(page_num)
+    )
+  elif mode == 'last':
+    sql = (
+        sql
+        .where(and_(table.c.fulfill.is_(fulfill),
+                    table.c.openid == openid))
+        .order_by(nulls_last(desc(table.c.last_time)))
+        .limit(page_num)
+    )
+  res = engine.execute(sql).fetchall()
+  result = {i: list() for i in WISH_FIELDS}
+  for i in res:
+    for idx, k in enumerate(WISH_FIELDS):
+      result[k].append(i[idx])
+  return make_succ_response(result)
+
+
+class WishUpdate(Schema):
+  wish_id = fields.Integer(required=True, validate=[Range(min=0)])
+  fulfill = fields.Boolean(load_default=None)
+  count = fields.Integer(load_default=1, 
+                         validate=[Range(min=0, max=10)])
+
+
+@app.route('/api/wooden_fish/wish/update',
+           methods=['POST'])
+@use_kwargs(WishUpdate)
+def wish_list(wish_id: int, fulfill: bool, count: int):
+  engine: Engine = db.engine
+  table = wish_table.table
+
+  values = dict(
+    count=table.c.count + count
+  )
+  if fulfill is not None:
+    values['fulfill'] = fulfill
+  
+  engine.execute(
+      table.update().values(**values).where(table.c.id == wish_id)
+  )
+  return make_succ_response({'result': True})
