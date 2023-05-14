0a2e11d6d920d41d5d02394fc2515dfe6c5534ec
zzzzzzian-20 <133369151+zzzzzzian-20@users.noreply.github.com>
2023-05-14 14:57:20 +0800

    Update view_daily_record.py

--- wxcloudrun/view_daily_record.py
+++ wxcloudrun/view_daily_record.py
@@ -2,11 +2,13 @@ from flask import request
 from marshmallow import Schema, fields
 from marshmallow.validate import Length
 from sqlalchemy.engine import Engine
+from sqlalchemy import select
+from sqlalchemy import desc
 from webargs.flaskparser import use_kwargs
 
 from run import app
 from wxcloudrun import db
-from wxcloudrun.tables import wish as wish_table
+from wxcloudrun.tables import wish_table
 from wxcloudrun.response import make_succ_response
 
 
@@ -22,10 +24,16 @@ def new_wish(wish: str):
   table = wish_table.table
   openid = request.headers.get('X-WX-OPENID')
 
-  res = engine.execute(
-      table.insert().values(
-          openid=openid,
-          wish=wish,
-      ).returning(table.c.id)
-  ).fetchall()
+  with engine.begin() as conn:
+    conn.execute(
+        table.insert().values(
+            openid=openid,
+            wish=wish,
+        )
+    )
+    res = conn.execute(
+        select(table.c.id)
+        .order_by(desc(table.c.create_time))
+        .limit(1)
+    ).fetchall()
   return make_succ_response({'id': res[0][0]})
