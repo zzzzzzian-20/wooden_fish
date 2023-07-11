c873653d724f25bcfda555192d5c9041f4a1399c
yipeiz <zhangyipei@metrodata.cn>
2023-07-11 14:56:25 +0800

    refactor: share knock

--- wxcloudrun/tables/share_knock.py
+++ wxcloudrun/tables/share_knock.py
@@ -1,27 +1,23 @@
 from sqlalchemy import Table
 from sqlalchemy import MetaData
-from sqlalchemy import Boolean
 from sqlalchemy import Column
 from sqlalchemy import Integer
 from sqlalchemy import TIMESTAMP
 from sqlalchemy import String
 from sqlalchemy import text
-from sqlalchemy import Boolean
 
 
 table = Table(
-    'wish_share',
+    'share_knock',
     MetaData(),
     Column('id', Integer, primary_key=True, index=True),
-    Column('share_id', String, unique=True, index=True),
-    Column('wish_id',
-           Integer,
+    Column('wish_id', Integer, index=True),
+    Column('create_time',
+           TIMESTAMP,
+           server_default=text('CURRENT_TIMESTAMP'),
            index=True,
            nullable=False),
-    Column('create_time',
-           TIMESTAMP(),
-           server_default=text('CURRENT_TIMESTAMP'),
-           nullable=False),
-    Column('share_content', Boolean, nullable=False),
-    Column('wish', String, nullable=True)
+    Column('openid', String, nullable=False),
+    Column('count', Integer, nullable=True),
+    Column('knock', Integer, nullable=True)
 )
--- wxcloudrun/tables/wish.py
+++ wxcloudrun/tables/wish.py
@@ -23,11 +23,17 @@ table = Table(
            nullable=False),
     Column('last_time', 
            TIMESTAMP(), 
-           server_onupdate=text('CURRENT_TIMESTAMP'),
-           nullable=True),
+           server_default=text('CURRENT_TIMESTAMP'),
+           nullable=False),
     Column('openid', String, nullable=True),
     Column('count', Integer, nullable=True),
     Column('knock', Integer, nullable=True),
     Column('fulfill', Boolean, nullable=True),
-    Column('wish', String, nullable=True)
+    Column('wish', String, nullable=True),
+    Column('helper_total', Integer, nullable=True),
+    Column('share_count_total', Integer, nullable=True),
+    Column('share_knock_total', Integer, nullable=True),
+    Column('helper', Integer, index=True, nullable=True),
+    Column('share_count', Integer, nullable=True),
+    Column('share_knock', Integer, nullable=True)
 )
